3555166975b9e750b1202f24d6b3cda5d2270dd4
Tareeq Hamilton <tareeqhamilton@gmail.com>
2021-01-28 16:12:30 -0500

    added to navbar, post and home pages

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,5 +1,17 @@
 import React from "react"
+import image from "../background1.jpg"
 
 export default function Home() {
-    return <h1> Home Page! </h1>
+    return (
+        <main>
+            <img 
+            src={image} 
+            alt="Computer Desk" 
+            className="absolute object-cover w-full h-full"
+            />
+                <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                    <h1 className="text-6xl text-blue-700 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Tareeq</h1>   
+                </section>            
+        </main>
+    )
 }
